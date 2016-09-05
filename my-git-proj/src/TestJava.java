ef52dc3c1ceffb9ed24a8201379d0c0e8d277b58
lvwenfei2002 <lvwenfei2002@163.com>
2016-09-05 11:25:17 +0800

    lvwenfei2002  2222

--- my-git-proj/src/TestJava.java
+++ my-git-proj/src/TestJava.java
@@ -2,5 +2,8 @@
 public class TestJava {
   public static void main (String [] args) {
 	  String test2221 = "111";
+	  // 测试新增信息
+	  System.out.println("1111111111");
+	  System.out.println("1111111112");
   }
 }
